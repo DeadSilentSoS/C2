248fb1969647e5a1dbf23d84a95d0423f4fb92ea
John Smith <deadsilentsos@gmail.com>
2023-09-15 15:23:06 -0500

    third commit

--- listeningpost/listening_post.py
+++ listeningpost/listening_post.py
@@ -6,7 +6,7 @@ LISTENING_PORT = 8081
 
 # Create the main application window
 root = tk.Tk()
-root.title("Concussion Listening Post")
+root.title("Concussion C2 Listening Post")
 root.geometry("400x300")  # Adjust the window size as needed
 
 # 1. Use a Consistent Color Scheme
@@ -18,35 +18,37 @@ text_color = "#333333"
 root.configure(bg=background_color)
 
 # 2. Add Labels and Headings
-main_heading = tk.Label(root, text="Concussion Listening Post", font=("Helvetica", 16, "bold"), bg=primary_color, fg=text_color)
+main_heading = tk.Label(root, text="Concussion C2 Listening Post", font=("Helvetica", 16, "bold"), bg=primary_color, fg=text_color)
 main_heading.pack(pady=(20, 10))
 
-# Function to start the listening post
-def start_listening_post():
-    # Add code to start the listening post
-    # You can use the LISTENING_IP and LISTENING_PORT here
-    status_label.config(text="Listening Post Status: Running")
-
-# Function to handle icon button event
-def icon_button_event():
-    # Add code to handle the icon button click event
-    pass
-
 # Create a label for the listening post status
-status_label = tk.Label(root, text="Listening Post Status: Not Running", bg=background_color)
+status_label = tk.Label(root, text="Listening Post Status: Running", bg=background_color)
 status_label.pack(pady=10)
 
-# Create a button to start the listening post
-start_button = tk.Button(root, text="Start Listening Post", bg=secondary_color, fg=text_color, command=start_listening_post)
+# 3. Improve Button Styling
+start_button = tk.Button(root, text="Start Listening", bg=secondary_color, fg=text_color, command=start_listening)
 start_button.pack()
 
-# 4. Incorporate Icons (Replace with your icon)
+# Placeholder for the start_listening function
+def start_listening():
+    # Placeholder for listening post start logic
+    status_label.config(text="Listening Post Status: Listening")
+
+# 4. Enhance Entry Fields
+command_entry = tk.Entry(root, width=40, font=("Helvetica", 12), bg="#ffffff")
+command_entry.pack()
+
+# 5. Incorporate Icons (Replace with your icon path)
 icon_image = tk.PhotoImage(file="C:/Users/noone/Desktop/Projects/C2/Misc/icon.png")
 icon_button = tk.Button(root, image=icon_image, text="Icon Button", compound="left", bg=primary_color, fg=text_color, command=icon_button_event)
 icon_button.image = icon_image  # Store a reference to the image
 icon_button.pack(pady=20)
 
+# Placeholder for the icon_button_event function
+def icon_button_event():
+    # Add code to handle the icon button click event
+    pass
+
 # Run the GUI application
 root.mainloop()
-
 
--- server/server.py
+++ server/server.py
@@ -14,14 +14,23 @@ root.geometry("400x300")  # Adjust the window size as needed
 # 1. Use a Consistent Color Scheme
 primary_color = "#3498db"
 secondary_color = "#2ecc71"
-background_color = "#f2f2f2"
+background_color = "#000080"
 text_color = "#333333"
 
 root.configure(bg=background_color)
 
 # 2. Add Labels and Headings
-main_heading = tk.Label(root, text="Concussion C3 Server", font=("Helvetica", 16, "bold"), bg=primary_color, fg=text_color)
+main_heading = tk.Label(root, text="Concussion C2 Server", font=("Helvetica", 16, "bold"), bg=primary_color, fg=text_color)
 main_heading.pack(pady=(20, 10))
+
+# Placeholder for the start_server function
+def start_server():
+    # Placeholder for server start logic
+    status_label.config(text="Server Status: Running")
+
+def icon_button_event():
+    # Add code to handle the icon button click event
+    pass
 
 # Create a label for the server status
 status_label = tk.Label(root, text="Server Status: Not Running", bg=background_color)
@@ -40,15 +49,6 @@ icon_image = tk.PhotoImage(file="C:/Users/noone/Desktop/Projects/C2/Misc/icon.pn
 icon_button = tk.Button(root, image=icon_image, text="Icon Button", compound="left", bg=primary_color, fg=text_color, command=icon_button_event)
 icon_button.image = icon_image  # Store a reference to the image
 icon_button.pack(pady=20)
-
-# Placeholder for the start_server function
-def start_server():
-    # Placeholder for server start logic
-    status_label.config(text="Server Status: Running")
-
-def icon_button_event():
-    # Add code to handle the icon button click event
-    pass
 
 # Run the GUI application
 root.mainloop()
